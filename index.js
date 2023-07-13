4a186c4a71a25b19b1db15373680fcefc4a5535b
Roman Ratushnyi <freshcode1001@ukr.net>
2023-07-13 18:28:50 +0300

    Props examples

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 class HeaderComponent extends React.Component {
     // тут може бути якийсь ще метод
     render() {
+        console.log(this.props);
+        // this.props.text = 'test'; --->>> Змінювати пропси заборонено!
         const h1 = React.createElement('h1', {}, 'Super Component!');
-        const h2 = React.createElement('h2', {title: 'Hi!'}, 'Header');
+        const h2 = React.createElement('h2', {title: 'Hi!'}, `Hello, ${this.props.text}`);
         const p = React.createElement('p', {}, 'Super paragraph');
 
         const article = React.createElement('article', {}, h1, h2, p);
-        
+
         return article;
     }
 }
 
-const component = React.createElement(HeaderComponent);
+const component1 = React.createElement(HeaderComponent, {text: 'React'});
+const component2 = React.createElement(HeaderComponent, {text: 'JavaScript'});
+const parentElement = React.createElement('section', {}, component1, component2);
 
 const root = document.querySelector('#root');
 
-ReactDOM.render(component, root);
+ReactDOM.render(parentElement, root);
