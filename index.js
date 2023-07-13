e1afa08bd44a6e354bc856b02278ad5be3f6dcca
Roman Ratushnyi <freshcode1001@ukr.net>
2023-07-13 18:14:15 +0300

    Class Component

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
+class HeaderComponent extends React.Component {
+    // тут може бути якийсь ще метод
+    render() {
+        const h1 = React.createElement('h1', {}, 'Super Component!');
+        const h2 = React.createElement('h2', {title: 'Hi!'}, 'Header');
+        const p = React.createElement('p', {}, 'Super paragraph');
+
+        const article = React.createElement('article', {}, h1, h2, p);
+        
+        return article;
+    }
+}
+
+const component = React.createElement(HeaderComponent);
+
 const root = document.querySelector('#root');
-// createElement(type, props, ...childrens)
-// type - тип елемента який потрібно створити
-// props - налаштування, просто передаємо {}
-// ...childrens - дочірні вузли
-const h1 = React.createElement('h1', {}, 'Hello, React!');
-console.log(h1);
-const anotherH1 = document.createElement('h1');
-console.dir(anotherH1);
 
-// render(element, container)
-// element - який елемент нам потрібно відрендерити
-// container - куди нам цей елемент потрібно приапендити
-ReactDOM.render(h1, root);
-
-/*
-
-Зареквайріть Реакт і Реакт-ДОМ (скопіюйте посилання CDN з мого коміту), 
-створіть елемент і вбудуйте його на сторінку
-
-*/
+ReactDOM.render(component, root);
