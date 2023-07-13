b0b63460c5faf2f627f3cc72efbea01d769c9b06
Roman Ratushnyi <freshcode1001@ukr.net>
2023-07-13 19:15:59 +0300

    State

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-class HeaderComponent extends React.Component {
-    // тут може бути якийсь ще метод
+class Counter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0
+        };
+    }
+
+    increment() {
+        // todo: state.count++;
+        console.log('CLICK');
+        this.setState({
+            count: this.state.count + 1
+        })
+    }
+    
     render() {
-        console.log(this.props);
-        // this.props.text = 'test'; --->>> Змінювати пропси заборонено!
-        const h1 = React.createElement('h1', {}, 'Super Component!');
-        const h2 = React.createElement('h2', {title: 'Hi!'}, `Hello, ${this.props.text}`);
-        const p = React.createElement('p', {}, 'Super paragraph');
-
-        const article = React.createElement('article', {}, h1, h2, p);
-
-        return article;
+        const h2 = React.createElement('h2', {}, this.state.count);
+        const button = React.createElement('button', {onClick: () => {this.increment()}}, '+');
+        
+        return React.createElement(React.Fragment, {}, h2, button);
     }
 }
 
-const component1 = React.createElement(HeaderComponent, {text: 'React'}, 'Text');
-const component2 = React.createElement(HeaderComponent, {text: 'JavaScript'});
-const parentElement = React.createElement('section', {}, component1, component2);
+const component = React.createElement(Counter);
 
 const root = document.querySelector('#root');
-
-ReactDOM.render(parentElement, root);
-
-
+ReactDOM.render(component, root);
