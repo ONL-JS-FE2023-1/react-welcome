76b49c2cf888508bdfb58177d3e9e70c34ca7d75
Roman Ratushnyi <freshcode1001@ukr.net>
2023-07-13 20:49:22 +0300

    Add Counter component

--- src/index.js
+++ src/index.js
@@ -1,39 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-class Counter extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            count: 0
-        };
-    }
-
-    increment() {
-        this.setState({
-            count: this.state.count + 1
-        })
-    }
-
-    decrement() {
-        if(this.state.count > 0) {
-            this.setState({
-                count: this.state.count - 1
-            })
-        } else if (this.state.count === 0) {
-            this.setState({
-                count: 0
-            })
-        }
-    }
-
-    render() {
-        const h2 = React.createElement('h2', {}, this.state.count);
-        const buttonIncrement = React.createElement('button', { onClick: () => { this.increment() } }, '+');
-        const buttonDecrement = React.createElement('button', { onClick: () => { this.decrement() } }, '-');
-        return React.createElement(React.Fragment, {}, h2, buttonIncrement, buttonDecrement);
-    }
-}
+import Counter from './components/Counter.js'
 
 const component = React.createElement(Counter);
 
