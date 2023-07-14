971b182a68f48ae9f0bf8d576a1ef886f2d0940c
Roman Ratushnyi <freshcode1001@ukr.net>
2023-07-14 18:25:27 +0300

    ES modules

---
+++ src/components/Math.js
@@ -0,0 +1,14 @@
+export const sum = (a,b) => a + b; // export -> іменований експорт
+
+export const sub = (a,b) => a - b;
+
+export const multy = (a,b) => a * b;
+
+export const div = (a,b) => a/b;
+
+export function superFunc() {
+    //alert('Hello world!');
+}
+
+
+// export default {sum, sub, multy, div}; // -> експорт по дефолту
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Counter from './components/Counter.js'
+import Counter from './components/Counter.js';
+// import MyMath from './components/Math.js'; -> експорт по дефолту
+import * as MyMath from './components/Math.js'; // -> іменований експорт
+
+MyMath.superFunc();
 
 const component = React.createElement(Counter);
 
